4e4550b997941448a7fbf9c528c5a098612942de
Javier Mengual <javiermengual@live.com>
2024-10-02 11:50:51 +0200

    Ejercicio 6 solucion

--- src/main/java/com/javiermengual/tema03/Ejercicio6.java
+++ src/main/java/com/javiermengual/tema03/Ejercicio6.java
@@ -1,4 +1,50 @@
 package com.javiermengual.tema03;
 
+import java.util.Scanner;
+
 public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+
+        System.out.println("Dime el dinero");
+        int centimos = (int)(scanner.nextDouble() * 100);
+
+        int totalMonedas = 0;
+
+        totalMonedas += centimos / 200;
+        centimos %= 200;
+
+        totalMonedas += centimos / 100;
+        centimos %= 100;
+
+        totalMonedas += centimos / 50;
+        centimos %= 50;
+
+        totalMonedas += centimos / 20;
+        centimos %= 20;
+
+        totalMonedas += centimos / 10;
+        centimos %= 10;
+
+        totalMonedas += centimos / 5;
+        centimos %= 5;
+
+        totalMonedas += centimos / 2;
+        centimos %= 2;
+
+        totalMonedas += centimos;
+
+        System.out.println("El número mínimo de monedas necesarias es: " + totalMonedas);
+
+        scanner.close();
+
+    }
+
+
+
+
+
+
+
 }

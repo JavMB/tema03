30fb107f15e9f30ca4b5285cb2a3eccce8a978de
Javier Mengual <javiermengual@live.com>
2024-10-03 10:22:22 +0200

    Ejercicios 8,9

--- src/main/java/com/javiermengual/tema03/Ejercicio8.java
+++ src/main/java/com/javiermengual/tema03/Ejercicio8.java
@@ -1,4 +1,38 @@
 package com.javiermengual.tema03;
 
+import java.util.Scanner;
+
 public class Ejercicio8 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int caraDado;
+
+
+        System.out.println("Que cara ha salido?");
+        caraDado=Integer.parseInt(scanner.nextLine());
+
+        caraDado = switch (caraDado){
+            case 1 -> 6;
+            case 2 ->5;
+            case 3->4;
+            case 6->1;
+            case 5->2;
+            case 4->3;
+            default -> 0;
+
+            /*
+            case 1,6 -> System.out.println(caraDado==1?1:6);
+
+            case 2,5-> System.out.println(caraDado==2?2:5);
+
+            case 3,4-> System.out.println(caraDado==3?3:4);
+
+            default -> System.out.println("ERROR");
+
+             */
+        };
+        System.out.println(caraDado);
+
+    }
 }

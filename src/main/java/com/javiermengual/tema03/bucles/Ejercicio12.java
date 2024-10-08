b58aa1634fa2046a22c3098627d943d28e24182d
Javi <javiermengual@live.com>
2024-10-08 19:28:53 +0200

    Ejercicio12

---
+++ src/main/java/com/javiermengual/tema03/bucles/Ejercicio12.java
@@ -0,0 +1,37 @@
+package com.javiermengual.tema03.bucles;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Random rand = new Random();
+        int intentos;
+        int contadorintentos=0;
+        int numeroAleatorio = rand.nextInt(100);
+        int numeroUsuario;
+
+        System.out.println("Dime cuantos intentos");
+        intentos=Integer.parseInt(sc.nextLine());
+
+        do {
+            System.out.println("Dime el numero");
+            numeroUsuario=Integer.parseInt(sc.nextLine());
+
+            if (numeroUsuario==numeroAleatorio){
+                System.out.println("HAS GANADO");
+                break;
+            }else {
+                System.out.println(numeroUsuario<numeroAleatorio?"es mas alto":"es mas bajo");
+                contadorintentos++;
+
+            }
+
+        }while (contadorintentos<=intentos);
+
+        if (numeroUsuario!=numeroAleatorio){
+            System.out.println("HAS PERDIDO");
+        }
+    }
+}

3a66fbe99b23d8c047d1c9afbeef2f1f2b192226
Javi <javiermengual@live.com>
2024-10-10 19:01:02 +0200

    Corregido 12

--- src/main/java/com/javiermengual/tema03/bucles/Ejercicio12.java
+++ src/main/java/com/javiermengual/tema03/bucles/Ejercicio12.java
@@ -5,32 +5,32 @@ import java.util.Scanner;
 
 public class Ejercicio12 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         Random rand = new Random();
         int intentos;
-        int contadorintentos=0;
+        int contadorintentos = 0;
         int numeroAleatorio = rand.nextInt(101);
         int numeroUsuario;
 
         System.out.println("Dime cuantos intentos");
-        intentos=Integer.parseInt(sc.nextLine());
+        intentos = Integer.parseInt(sc.nextLine());
 
         do {
             System.out.println("Dime el numero");
-            numeroUsuario=Integer.parseInt(sc.nextLine());
+            numeroUsuario = Integer.parseInt(sc.nextLine());
 
-            if (numeroUsuario==numeroAleatorio){
+            if (numeroUsuario == numeroAleatorio) {
                 System.out.println("HAS GANADO");
                 break;
-            }else {
-                System.out.println(numeroUsuario<numeroAleatorio?"es mas alto":"es mas bajo");
+            } else {
+                System.out.println(numeroUsuario < numeroAleatorio ? "es mas alto" : "es mas bajo");
                 contadorintentos++;
 
             }
 
-        }while (contadorintentos<intentos);
+        } while (contadorintentos < intentos);
 
-        if (numeroUsuario!=numeroAleatorio){
+        if (numeroUsuario != numeroAleatorio) {
             System.out.println("HAS PERDIDO");
         }
     }

72dff748077d991700e3cb0e4368c108e6ca03e6
Javi <javiermengual@live.com>
2024-10-13 19:07:05 +0200

    15

---
+++ src/main/java/com/javiermengual/tema03/bucles/Ejercicio15.java
@@ -0,0 +1,30 @@
+package com.javiermengual.tema03.bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio15 {
+    // Escribe un programa que solicite 3 números enteros h, m y s que contengan la hora, minutos y
+    //segundos respectivamente, y compruebe si la hora que indican estos valores es un hora válida.
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int h,m,s;
+        System.out.println("Dime la hora, minutos y segundos");
+        h=Integer.parseInt(sc.nextLine());
+        m=Integer.parseInt(sc.nextLine());
+        s=Integer.parseInt(sc.nextLine());
+
+        if (h>24||m>60||s>60){
+            System.out.println("Hora incorrecta");
+        }else {
+            System.out.println("Horra correcta");
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/com/javiermengual/tema03/bucles/Ejercicio16.java
@@ -0,0 +1,34 @@
+package com.javiermengual.tema03.bucles;
+
+public class Ejercicio16 {
+    public static void main(String[] args) {
+        /*Escribe un programa que permita jugar al siguiente juego. Se dispone de un dado normal de 6
+caras con valores comprendidos entre 1 y 6. El objetivo del juego es sumar más puntos que el rival
+(en este caso el ordenador) sin pasarse de 11 puntos (similitudes con el blackjack). Empezará el
+jugador, que lanzará los dados hasta que ocurra alguno de los siguientes acontecimientos:
+
+a) Obtenga exactamente 11 puntos (ha ganado).
+b) Decida plantarse con una puntuación inferior a 11 puntos.
+c) Se haya pasado de 11 puntos y por tanto habrá perdido.
+
+En el caso a) sumará automáticamente dos porras (1 porra por victoria y otra de bonificación por
+alcanzar la puntuación máxima).
+En el caso b) el ordenador jugará e intentará igualar o superar sin pasarse de 11 puntos la
+puntuación del jugador. Si consigue igualar o superar la puntuación sumará una porra para el
+ordenador. Si además lo hace con una puntuación de 11 puntos tendrá bonificación de otra porra
+por alcanzar la puntuación máxima.
+En el caso c) el ordenador gana automáticamente y suma una porra sin necesidad de jugar.
+El primero que llegue a 5 porras gana la partida*/
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

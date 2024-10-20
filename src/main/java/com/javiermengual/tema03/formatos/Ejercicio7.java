e74f5ff59e50a7bf75503f85a589eb7ba9564605
Javi <javiermengual@live.com>
2024-10-20 21:39:51 +0200

    7 corregido

--- src/main/java/com/javiermengual/tema03/formatos/Ejercicio7.java
+++ src/main/java/com/javiermengual/tema03/formatos/Ejercicio7.java
@@ -2,17 +2,19 @@ package com.javiermengual.tema03.formatos;
 
 public class Ejercicio7 {
     public static void main(String[] args) {
-        String ascendientes = "";
+//        String ascendientes = "";
+//
+////        for (int i = 1; i <= 9; i++) {
+////            ascendientes = ascendientes + i;
+////            System.out.printf("%9s", ascendientes); //%alinea derecha %- alinea izquierda
+////
+////            for (int j = i; j > 0; j--) {
+////                System.out.printf("%s", j);
+////            }
+////            System.out.println();
+////        }
 
-        for (int i = 1; i <= 9; i++) {
-            ascendientes = ascendientes + i;
-            System.out.printf("%9s", ascendientes); //%alinea derecha %- alinea izquierda
 
-            for (int j = i; j > 0; j--) {
-                System.out.printf("%s", j);
-            }
-            System.out.println();
-        }
 
 
 
